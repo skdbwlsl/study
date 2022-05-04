efdfa478dd93dfb5821cce82205adf4af69facf5
dbwls <youjin9874@naver.com>
2022-05-04 16:24:51 +0900

    for 반복문

---
+++ algorithm/src/chap01/sumFor1_2.java
@@ -0,0 +1,23 @@
+package chap01;
+
+import java.util.Scanner;
+
+public class sumFor1_2 {
+	//Q1. n = 7이면 1+2+3+4+5+6+7=28로 출력하는 프로그램을 작성하세요
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int n;
+		int sum=0;
+		System.out.println("1부터 7까지의 합을 구합니다");
+		System.out.print("n의 값 : ");
+		n= input.nextInt();
+
+		for(int i =1;i<=7;i++) {
+			sum += i;
+		}
+		System.out.println("1 + 2 + 3 + 4 + 5 + 6 + 7 = " + sum  );
+
+	}
+
+}
